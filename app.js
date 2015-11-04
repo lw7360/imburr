1ff40610943efe208689973129554fceec15693d
Larry Wu <lw7360@gmail.com>
2015-11-04 09:41:30 -0800

    Use custom titlebar

--- app.js
+++ app.js
@@ -1,71 +1,48 @@
-var Bacon = require('baconjs').Bacon;
-var $ = require('jquery');
-$.fn.asEventStream = Bacon.$.asEventStream;
-var open = require('open');
-var store = require('store');
+'use strict';
+const app = require('app');
+const BrowserWindow = require('browser-window');
 
-if (!store.get('uploads')) {
-  store.set('uploads', []);
+// report crashes to the Electron project
+require('crash-reporter').start();
+
+// adds debug features like hotkeys for triggering dev tools and reload
+require('electron-debug')();
+
+// prevent window being garbage collected
+let mainWindow;
+
+function onClosed () {
+  // dereference the window
+  // for multiple windows store them in an array
+  mainWindow = null;
 }
 
-var clientID = '69eb9f84f6aff83';
-var holder = document.documentElement;
-
-function readfiles (files) {
-  for (var i = 0; i < files.length; i++) {
-    var formData = new FormData();
-    formData.append('image', files[i]);
-
-    $.ajax({
-      url: 'https://api.imgur.com/3/image',
-      type: 'POST',
-      datatype: 'json',
-      headers: {
-        'Authorization': 'Client-ID ' + clientID
-      },
-      data: formData,
-      success: function (response) {
-        if (response.success) {
-          store.set('uploads', store.get('uploads').concat(response.data.link));
-          open(response.data.link);
-          showUploads();
-        } else {
-          alert("Couldn't upload");
-        }
-      },
-      error: function (xhr, ajaxOptions, thrownError) {
-        alert("Couldn't upload");
-      },
-      cache: false,
-      contentType: false,
-      processData: false
-    });
-  }
-}
-
-function showUploads() {
-  Bacon.fromArray(store.get('uploads')).onValue(function (value) {
-    console.log(value);
+function createMainWindow () {
+  const win = new BrowserWindow({
+    width: 300,
+    height: 300,
+    resizable: false,
+    frame: false
   });
+
+  win.loadUrl('file://' + __dirname + '/index.html');
+  win.on('closed', onClosed);
+
+  return win;
 }
-showUploads();
 
-holder.ondragover = function (e) {
-  e.preventDefault();
-  $('#instructions').addClass('animated infinite shake');
-};
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
 
-holder.ondragend = function () {
-  $('#instructions').removeClass('animated infinite shake');
-};
+app.on('activate-with-no-open-windows', () => {
+  if (!mainWindow) {
+    mainWindow = createMainWindow();
+  }
+});
 
-holder.ondragleave = function () {
-  $('#instructions').removeClass('animated infinite shake');
-};
-
-var drop = $(holder).asEventStream('drop').doAction('.preventDefault');
-
-drop.onValue(function(e) {
-  $('#instructions').removeClass('animated infinite shake');
-  readfiles(e.originalEvent.dataTransfer.files);
+app.on('ready', () => {
+  mainWindow = createMainWindow();
 });
--- index.js
+++ index.js
@@ -1,46 +1,63 @@
-'use strict';
-const app = require('app');
-const BrowserWindow = require('browser-window');
+var Bacon = require('baconjs').Bacon;
+var $ = require('jquery');
+$.fn.asEventStream = Bacon.$.asEventStream;
+var open = require('open');
+var titlebar = require('titlebar')();
 
-// report crashes to the Electron project
-require('crash-reporter').start();
-
-// adds debug features like hotkeys for triggering dev tools and reload
-require('electron-debug')();
-
-// prevent window being garbage collected
-let mainWindow;
-
-function onClosed () {
-  // dereference the window
-  // for multiple windows store them in an array
-  mainWindow = null;
+if (process.platform !== 'win32') {
+  titlebar.appendTo('#titlebar');
+  $('.titlebar-fullscreen').empty();
 }
 
-function createMainWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600
-  });
+var clientID = '69eb9f84f6aff83';
+var holder = document.documentElement;
 
-  win.loadUrl('file://' + __dirname + '/index.html');
-  win.on('closed', onClosed);
+function readfiles (files) {
+  for (var i = 0; i < files.length; i++) {
+    var formData = new FormData();
+    formData.append('image', files[i]);
 
-  return win;
+    $.ajax({
+      url: 'https://api.imgur.com/3/image',
+      type: 'POST',
+      datatype: 'json',
+      headers: {
+        'Authorization': 'Client-ID ' + clientID
+      },
+      data: formData,
+      success: function (response) {
+        if (response.success) {
+          open(response.data.link);
+        } else {
+          alert("Couldn't upload");
+        }
+      },
+      error: function (xhr, ajaxOptions, thrownError) {
+        alert("Couldn't upload");
+      },
+      cache: false,
+      contentType: false,
+      processData: false
+    });
+  }
 }
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+holder.ondragover = function (e) {
+  e.preventDefault();
+  $('#instructions').addClass('animated infinite shake');
+};
 
-app.on('activate-with-no-open-windows', () => {
-  if (!mainWindow) {
-    mainWindow = createMainWindow();
-  }
-});
+holder.ondragend = function () {
+  $('#instructions').removeClass('animated infinite shake');
+};
 
-app.on('ready', () => {
-  mainWindow = createMainWindow();
+holder.ondragleave = function () {
+  $('#instructions').removeClass('animated infinite shake');
+};
+
+var drop = $(holder).asEventStream('drop').doAction('.preventDefault');
+
+drop.onValue(function(e) {
+  $('#instructions').removeClass('animated infinite shake');
+  readfiles(e.originalEvent.dataTransfer.files);
 });
